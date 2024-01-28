936845ba641e11d309b57c5c513c24e539f1c1d1
Paritosh <paritosh.sharmas@gmail.com>
2024-01-28 21:02:37 +0100

    resolution fixes

--- scenes/strongman scene/HUD.gd
+++ scenes/strongman scene/HUD.gd
@@ -42,3 +42,7 @@ func _on_start_button_pressed():
 
 func _on_message_timer_timeout():
 	$Message.hide()
+
+
+func _on_backbutton_pressed():
+	get_tree().change_scene_to_file("res://scenes/menu2.tscn")
