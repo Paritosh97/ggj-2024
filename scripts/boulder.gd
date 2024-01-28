10c7d9fc5afe8b9928f572c5b816ffa494d4c651
Paritosh <paritosh.sharmas@gmail.com>
2024-01-28 02:12:52 +0100

    rigged horse with animation

---
+++ scripts/boulder.gd
@@ -0,0 +1,11 @@
+extends RigidBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	contact_monitor = true
+	max_contacts_reported = 10
+
+func _on_body_exited(body):
+	if body is CharacterBody2D:
+		linear_velocity = Vector2(0, 0)
+	print("Here")
